40061c02ccd1104599581f84b55fb906e23c876b
Emmanuel Bourgerie <manu@bourgerie.fr>
2018-12-04 18:12:42 +0000

    Improved test coverage

--- test/pistolet.spec.ts
+++ test/pistolet.spec.ts
@@ -1,11 +1,28 @@
+import { Request, Response } from 'express';
 import { setConfig } from '../src/config';
 import { DefaultScenario } from '../src/default-scenario';
 import { Pistolet } from '../src/pistolet';
+import { Scenario } from '../src/scenario';
 
 describe('Pistolet', () => {
   let pistolet: Pistolet;
+  let spyResponse: Response;
+  let spyScenario: Scenario;
 
   beforeEach(() => {
+    spyScenario = {
+      mocks: [],
+      next: (req, res, match) => match,
+      reset: () => void 0,
+    };
+    spyOn(spyScenario, 'next');
+    spyOn(spyScenario, 'reset');
+
+    spyResponse = {
+      send: () => void 0,
+    } as Response;
+    spyOn(spyResponse, 'send');
+
     setConfig({
       dir: `${__dirname}/samples`,
     });
@@ -24,6 +41,45 @@ describe('Pistolet', () => {
       expect(scenario).toBeDefined();
       expect(scenario instanceof DefaultScenario).toBe(true);
       expect(scenario.mocks[0].request).toEqual({ method: 'GET', path: '/api/endpoint' });
+    });
+  });
+
+  describe('onRequest()', () => {
+    beforeEach(() => {
+      pistolet.loadScenarios([spyScenario, 'basic']);
+      (spyScenario.next as jasmine.Spy).calls.reset();
+    });
+
+    it('returns the first mock matching the request', () => {
+      const request = {
+        method: 'GET',
+        url: '/api/endpoint',
+      } as Request;
+      pistolet.onRequest(request, spyResponse);
+
+      expect(spyResponse.statusCode).toBe(200);
+      expect(spyResponse.send).toHaveBeenCalledWith('Hello, World!');
+
+      expect(spyScenario.next).not.toHaveBeenCalled();
+    });
+
+    it('returns 404 if not mock is found', () => {
+      const request = {
+        method: 'GET',
+        url: '/not/an/api/endpoint',
+      } as Request;
+      pistolet.onRequest(request, spyResponse);
+
+      expect(spyResponse.statusCode).toBe(404);
+      expect(spyResponse.send).toHaveBeenCalledWith('not found');
+    });
+  });
+
+  describe('reset()', () => {
+    it('should reset all scenarios', () => {
+      pistolet.loadScenarios([spyScenario]);
+      pistolet.reset();
+      expect(spyScenario.reset).toHaveBeenCalled();
     });
   });
 });
--- test/request-matcher.spec.ts
+++ test/request-matcher.spec.ts
@@ -23,6 +23,19 @@ describe('RequestMatcher', () => {
       expect(matcher.findMatch(request, [ basicRequest ])).toBe(basicRequest);
     });
 
+    it('should compare the mock and request bodies', () => {
+      const requestWithBody = {
+        request: {
+          body: { some: 'data' },
+          method: 'GET',
+          path: '/api/endpoint',
+        },
+        response: { data: 'Hello, World!' },
+      };
+      const request = partialRequest({ url: '/api/endpoint', method: 'GET', body: { some: 'data' } });
+      expect(matcher.findMatch(request, [ requestWithBody ])).toBe(requestWithBody);
+    });
+
     it('should return undefined if no mock matches the request', () => {
       const request = partialRequest({ url: '/api/different/endpoint', method: 'POST' });
       expect(matcher.findMatch(request, [ basicRequest ])).toBeUndefined();
