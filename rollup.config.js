94754fe141b5e929fcc79a525f0a91b71b03d5ad
Emmanuel Bourgerie <manu@bourgerie.fr>
2019-02-15 14:30:54 +0000

    Refactoring cleanup – Reintegrate typing files

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,42 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-const plugins = [
-  typescript({
-    useTsconfigDeclarationDir: true,
-    tsconfigOverride: {
-      compilerOptions: {
-      },
-      exclude: [ '**/*.spec.ts', 'examples/**/*.ts' ],
-    },
-  }),
+const external = [
+  'cors',
+  'debug',
+  'events',
+  'express',
+  'http',
+  'path',
 ];
 
 export default [{
   input: 'src/index.ts',
-  external: [
-    'debug',
-    'path',
+  external,
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true,
+        },
+        exclude: ['**/*.spec.ts', 'examples/**/*.ts'],
+      },
+    }),
   ],
-  plugins,
-  output: { file: pkg.main, format: 'cjs' },
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+  },
 }, {
   input: 'src/backends/express/index.ts',
-  external: [
-    'cors',
-    'debug',
-    'events',
-    'express',
-    'http',
-    'path',
+  external,
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        exclude: ['**/*.spec.ts', 'examples/**/*.ts'],
+      },
+    }),
   ],
-  plugins,
   output: {
     file: 'backends/express.js',
     format: 'cjs',
